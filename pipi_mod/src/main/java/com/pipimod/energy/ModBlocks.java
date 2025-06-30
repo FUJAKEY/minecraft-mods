8ffc2a5ecf0949c6d1799b0952ff213b3b366f1b
FUJAKEY <164060147+FUJAKEY@users.noreply.github.com>
2025-06-30 13:11:39 +0300

    Add drops and ore requirements

--- pipi_mod/src/main/java/com/pipimod/energy/ModBlocks.java
+++ pipi_mod/src/main/java/com/pipimod/energy/ModBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.common.ToolType;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, EnergyMod.MODID);
@@ -16,9 +17,21 @@ public class ModBlocks {
     public static final RegistryObject<Block> ENERGY_WIRE = BLOCKS.register("energy_wire", EnergyWireBlock::new);
 
     public static final RegistryObject<Block> COPPER_ORE = BLOCKS.register("copper_ore", () ->
-            new Block(Block.Properties.of(Material.STONE).strength(3.0F).requiresCorrectToolForDrops()));
+            new Block(Block.Properties.of(Material.STONE)
+                    .strength(3.0F)
+                    .harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(1)
+                    .requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> LEAD_ORE = BLOCKS.register("lead_ore", () ->
-            new Block(Block.Properties.of(Material.STONE).strength(3.0F).requiresCorrectToolForDrops()));
+            new Block(Block.Properties.of(Material.STONE)
+                    .strength(3.0F)
+                    .harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(1)
+                    .requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> OSMIUM_ORE = BLOCKS.register("osmium_ore", () ->
-            new Block(Block.Properties.of(Material.STONE).strength(3.0F).requiresCorrectToolForDrops()));
+            new Block(Block.Properties.of(Material.STONE)
+                    .strength(3.0F)
+                    .harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(1)
+                    .requiresCorrectToolForDrops()));
 }
