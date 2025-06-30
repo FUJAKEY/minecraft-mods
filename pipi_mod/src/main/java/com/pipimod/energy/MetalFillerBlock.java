eb8686868c3e5c40adc9ebed7597787c728b6b1c
FUJAKEY <164060147+FUJAKEY@users.noreply.github.com>
2025-06-30 15:14:58 +0300

    Fix Metal Filler GUI opening

--- pipi_mod/src/main/java/com/pipimod/energy/MetalFillerBlock.java
+++ pipi_mod/src/main/java/com/pipimod/energy/MetalFillerBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
@@ -11,6 +12,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 
@@ -34,9 +36,10 @@ public class MetalFillerBlock extends Block {
     public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, net.minecraft.util.math.BlockRayTraceResult hit) {
         if (!world.isClientSide) {
             TileEntity te = world.getBlockEntity(pos);
-            if (te instanceof INamedContainerProvider) {
-                player.openMenu((INamedContainerProvider) te);
+            if (te instanceof INamedContainerProvider && player instanceof ServerPlayerEntity) {
+                NetworkHooks.openGui((ServerPlayerEntity) player, (INamedContainerProvider) te, pos);
             }
+            return ActionResultType.CONSUME;
         }
         return ActionResultType.SUCCESS;
     }
